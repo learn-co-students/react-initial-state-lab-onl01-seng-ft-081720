03f79502b675e3e5b1d1c34907e935c6b2a706a7
Newland-A <ajn252@gmail.com>
2020-12-18 06:50:08 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,22 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+  constructor(props) {
+    super()
+
+    this.state = {
+      //define initial state with a key of 'someKey' set to the 'someValue' prop
+      secondsLeft: props.initialCount
+    }
+  }
+
+  render() {
+    const msg = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`
+
+    return (
+      // curly brackets needed to interpolate
+      <div>{msg}</div>
+    )
+  }
+}
